5ee7cf8b34b4e37406f94756c79e0a60417f1231
Sourabh Verma <sourabh.coder@gmail.com>
2014-09-23 22:38:51 +0530

    Add bot

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import setup
 
-setup(name='YourAppName',
+setup(name='Megabot',
       version='1.0',
       description='OpenShift App',
-      author='Your Name',
-      author_email='example@example.com',
-      url='http://www.python.org/sigs/distutils-sig/',
-#      install_requires=['Django>=1.3'],
+      author='Sourabh Verma',
+      author_email='sourabh.coder@gmail.com',
+      url='https://github.com/sourabhv/megabot',
+     install_requires=['pyopenssl', 'twisted'],
      )
